dbddf9ce4169c1b0fdbdd027a25a877f0c2f3e89
Robert Gabriel <97358106+RobertGabriel7@users.noreply.github.com>
2022-09-12 23:19:06 -0300

    atualizações do curso de php cod3r

---
+++ php CODER/projeto 1/Exercicios/exercicios.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Karla:wght@200;500&family=Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="../Curso PHP/style.css">
+    <title>EXERCICIO</title>
+</head>
+<body class="exercicio"> 
+ <header class="cabe">
+    <h1>Curso de PHP</h1>
+    <h2>Visualização de Exercicios</h2>
+ </header>
+ <nav class="navega" >
+   <a id="a1" href="#">SEM FORMATAÇÃO</a>
+   <a id="a2" href="../Curso PHP/site.php" class="vermelho">Voltar</a>
+ </nav>
+ <main class="prin">
+    <div class="conteudo">
+
+    </div>
+ </main>
+ <footer class="rodape">
+    DESENVOLVIDO POR ROBERT, COM COD3R. ©️ 
+    <!-- Data de hoje -->
+    <?php echo date('Y');?>
+   
+ </footer>
+</body>
+</html>
