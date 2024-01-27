88c7f48c6dea072627cce9d7d7186181fa7b80bd
Robert <97358106+RobertGabriel7@users.noreply.github.com>
2024-01-27 13:39:58 -0300

    more data

---
+++ PHP/php por GUA/PHP_POO/Aula_8/Figth.php
@@ -0,0 +1,26 @@
+<?php
+
+/* Relacionamento de agregração serve para agregar caracteristicas de uma class para outra */
+
+class Luta {
+
+/* Atributos */
+
+private $desafiado;
+private $desafiante;
+private $rounds;
+private $lutaAprovada;
+
+/* Method */
+
+public function marcarLuta(){
+
+}
+
+public function lutar(){
+    
+}
+
+}
+
+?>
